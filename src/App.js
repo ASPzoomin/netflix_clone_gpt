498c7c856c21f2b97532c5392cd362ee443d8700
pavan <pavanammula@gmail.com>
2023-11-19 13:06:51 +0530

    initial proj setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p className="text-xl font-bold text-red-800">Building Netflix GPT</p>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
